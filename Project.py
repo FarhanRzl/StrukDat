74fad78894b3e5e23590fa7568d454ba7b066386
FarhanRzl <faruqfarhan321@gmail.com>
2022-12-08 15:40:51 +0700

    new

--- Project.py
+++ Project.py
@@ -1,83 +1,83 @@
-# import heapq
-# import csv
+import heapq
+import csv
+import os 
+import time
 
-# login = []
-# data = []
-# count = 0
+login = []
+data = []
+count = 0
 
-# with open ('login.csv') as log:
-#     logs = csv.reader(log)
-#     for i in logs:
-#         login.append(i)
+with open ('login.csv') as log:
+    logs = csv.reader(log)
+    for i in logs:
+        login.append(i)
 
-# with open ('database.csv') as db:
-#     dbase = csv.reader(db)
-#     for i in dbase:
-#         data.append(i)
+with open ('database.csv') as db:
+    dbase = csv.reader(db)
+    for i in dbase:
+        data.append(i)
 
-# def Login():
-#     global count
-#     username = input('Masukkan Username : ')
-#     password = input('Masukkan Password : ')
+def Login():
+    global count
+    username = input('Masukkan Username : ')
+    password = input('Masukkan Password : ')
+    time.sleep(5)
+    os.system('cls')
 
-#     for i in range(len(login)-1):
-#         if username == login[i][0] and password == login[i][0]:
-#             Main()
-#         else:
-#             print('username atau password salah!\n')
-#             count+=1
-#             if count < 3:
-#                 Login()
+    for i in range(len(login)-1):
+        if username == login[i][0] and password == login[i][0]:
+            Main()
+        else:
+            print('username atau password salah!\n')
+            count+=1
+            if count < 3:
+                Login()
 
-# def Register():
-#     username = input('Masukkan Username : ')
-#     password = input('Masukkan Password : ')
-    
+def Register():
+    username = input('Masukkan Username : ')
+    password = input('Masukkan Password : ')
 
-# def Main():
-#     print('='*40)
-#     print("\t\t MENU")
-#     print('='*40)
-#     print('\t1. Show Data')
-#     print('\t2. Insert Data')
-#     print('\t3. Delete Data')
-#     print('='*40)
-#     choice = input('Pilih menu : ')
+    data.append([username, password])
 
-#     if choice == '1':
-#         Show()
-#     elif choice == '2':
-#         Insert()
-#     elif choice == '3':
-#         Delete()
-#     else:
-#         print("Menu tidak valid")
+    with open('database.csv', 'w') as dt:
+        dts = csv.writer(dt)
+        dts.writerows(data)
 
-# def Show():
-#     print("="*17, "DATA", "="*17)
-#     print ("{:<10} {:<15} {:<10} {:<10} {:<10} {:<10}".format('Barang','Tanggal','Pengirim','Penerima', 'Jarak', 'Layanan'))
-#     for k, v in data:
-#         barang, tanggal, pengirim, penerima, jarak, layanan = v
-#         print ("{:<10} {:<15} {:<10} {:<10} {:<10} {:<10}".format(k, barang, tanggal, pengirim, penerima, jarak, layanan))
-#     print("="*40)
-#     Main()
+def Main():
+    print('='*40)
+    print("\t\t MENU")
+    print('='*40)
+    print('\t1. Show Data')
+    print('\t2. Insert Data')
+    print('\t3. Delete Data')
+    print('='*40)
+    choice = input('Pilih menu : ')
 
-# def Insert():
-#     Main()
+    if choice == '1':
+        Show()
+    elif choice == '2':
+        Insert()
+    elif choice == '3':
+        Delete()
+    else:
+        print("Menu tidak valid")
 
-# def Delete():
-#     Main()
+def Show():
+    print("="*32, "DATA", "="*32)
+    print ("{:<16} {:<15} {:<15} {:<10} {:<8} {:<10}".format('Barang','Tanggal','Pengirim','Penerima', 'Jarak', 'Layanan'))
+    for i in range(len(data)-1):
+        print ("{:<13} {:<15} {:<15} {:<13} {:<10} {:<10}".format(data[i][0], data[i][1], data[i][2], data[i][3], data[i][4], data[i][5]))
+    print("="*70)
+    Main()
 
-# Login()
+def Insert():
+    item = input('Data yang dimasukkan : ')
+    heapq.heappush(data, item)
+    Main()
 
-d = {1: ["Python", 33.2, 'UP'],
-2: ["Java", 23.54, 'DOWN'],
-3: ["Ruby", 17.22, 'UP'],
-10: ["Lua", 10.55, 'DOWN'],
-5: ["Groovy", 9.22, 'DOWN'],
-6: ["C", 1.55, 'UP']
-}
-print ("{:<8} {:<15} {:<10} {:<10}".format('Pos','Lang','Percent','Change'))
-for k, v in d.items():
-    lang, perc, change = v
-    print ("{:<8} {:<15} {:<10} {:<10}".format(k, lang, perc, change))
+def Delete():
+    item = input('Data yang dihapus : ')
+    heapq.heappop(data)
+    Main()
+
+Login()
